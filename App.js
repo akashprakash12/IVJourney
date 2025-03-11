127fbe303d925a0cf95de4b36a111e54a206d6e7
akashprakash12 <akashprakash7032@gmail.com>
2025-03-11 21:01:44 +0530

    database and login updated

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import * as React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ThemeProvider } from "./context/ThemeContext";
-import { AuthProvider } from "./context/Authcontext"; // Import AuthProvider
-import AppNavigation from "./AppNavigation"; // Move AppNavigation to a separate file
+import { AuthProvider } from "./context/Authcontext";
+import AppNavigation from "./AppNavigation";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
     <AuthProvider>
       <ThemeProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <AppNavigation />
-        </GestureHandlerRootView>
+        <PaperProvider> {/* Wrap your app inside PaperProvider */}
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <AppNavigation />
+          </GestureHandlerRootView>
+        </PaperProvider>
       </ThemeProvider>
     </AuthProvider>
   );
