be99a772e3a586f27a184dd044dd7b52dcfdfdd4
S1D3KICK181 <mo.hamd.pvt@gmail.com>
2025-03-12 14:59:04 +0530

    modified

--- src/HOD/RequestApprove.js
+++ src/HOD/RequestApprove.js
@@ -56,17 +56,6 @@ export default function PDFPreview() {
   const updateStatus = async (userId, status) => {
     try {
       setLoading(true);
-<<<<<<< HEAD
-      
-      // Update the status in the backend
-      await axios.put(`http://${IP}:5000/api/request-status/${userId}`, { status });
-  
-      // Update the local state immediately
-      setStudentRequests(prevRequests => 
-        prevRequests.map(request => 
-          request.Obj_id._id === userId 
-            ? { ...request, Obj_id: { ...request.Obj_id, status } } 
-=======
   
       // Update the status in the backend
       await axios.put(`http://${IP}:5000/api/request-status/${userId}`, {
@@ -78,7 +67,6 @@ export default function PDFPreview() {
         prevRequests.map((request) =>
           request.Obj_id._id === userId
             ? { ...request, Obj_id: { ...request.Obj_id, status } }
->>>>>>> 48363e16221e26c9081b856dfac5b272da926fcd
             : request
         )
       );
